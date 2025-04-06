7b691033f848213159866c73d6d799c33531954f
hidehidev7 <hidehidev7@gmail.com>
2025-04-06 18:56:27 +0900

    色々

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 'title': 'Hide Server', 'pagetitle': 'トップ', });
 });
 
 router.get('/minigame/', function(req, res, next) {

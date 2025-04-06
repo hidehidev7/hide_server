48577e1ccec1d9292597b4fc4d76cc6c4da1dc5f
hidehidev7 <hidehidev7@gmail.com>
2025-04-06 19:37:39 +0900

    色々2

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 'title': 'Hide Server', 'pagetitle': 'トップ', });
+  res.render('index', { 'title': req.app.get("server name"), 'pagetitle': 'トップ', });
 });
 
 router.get('/minigame/', function(req, res, next) {
-  res.render('yoke-game');
+  res.render('yoke-game', { 'title': 'よけゲーム | ' + req.app.get("server name"), 'pagetitle': 'よけゲーム', });
 });
 
 module.exports = router;
